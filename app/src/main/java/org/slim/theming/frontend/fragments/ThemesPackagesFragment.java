abc6a5dc0202367e3bc1b7bfdbe894cfc4674dae
Victor Lapin <victor@slimroms.org>
2017-01-19 17:49:12 +0300

    implemented theme packages loading

--- app/src/main/java/org/slim/theming/frontend/fragments/ThemesPackagesFragment.java
+++ app/src/main/java/org/slim/theming/frontend/fragments/ThemesPackagesFragment.java
@@ -1,8 +1,12 @@
 package org.slim.theming.frontend.fragments;
 
+import android.content.*;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.RemoteException;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -10,9 +14,15 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import com.slimroms.themecore.IThemeService;
+import com.slimroms.themecore.Theme;
+import org.slim.theming.frontend.App;
 import org.slim.theming.frontend.R;
 import org.slim.theming.frontend.adapters.ThemesPackagesAdapter;
+import org.slim.theming.frontend.helpers.BroadcastHelper;
 import org.slim.theming.frontend.views.LineDividerItemDecoration;
+
+import java.util.List;
 
 public class ThemesPackagesFragment extends Fragment {
     public static final String TAG = ThemesPackagesFragment.class.getSimpleName();
@@ -43,5 +53,62 @@ public class ThemesPackagesFragment extends Fragment {
         mRecycler.setItemAnimator(new DefaultItemAnimator());
         mAdapter = new ThemesPackagesAdapter(getContext());
         mRecycler.setAdapter(mAdapter);
+    }
+
+    private BroadcastReceiver mEventReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            ComponentName backendName;
+
+            switch (intent.getAction()) {
+                case BroadcastHelper.ACTION_BACKEND_CONNECTED:
+                    backendName = intent.getParcelableExtra(BroadcastHelper.EXTRA_BACKEND_NAME);
+                    new AsyncTask<ComponentName, Void, List<Theme>>() {
+                        @Override
+                        protected List<Theme> doInBackground(ComponentName... componentNames) {
+                            if (componentNames.length > 0) {
+                                final IThemeService backend = App.getInstance().getBackend(componentNames[0]);
+                                try {
+                                    return backend.getThemePackages();
+                                }
+                                catch (RemoteException ex) {
+                                    ex.printStackTrace();
+                                    return null;
+                                }
+                            }
+                            else
+                                return null;
+                        }
+
+                        @Override
+                        protected void onPostExecute(List<Theme> themes) {
+                            if (themes != null) {
+                                mAdapter.addThemes(themes);
+                            }
+                        }
+                    }.execute(backendName);
+                    break;
+                case BroadcastHelper.ACTION_BACKEND_DISCONNECTED:
+                    backendName = intent.getParcelableExtra(BroadcastHelper.EXTRA_BACKEND_NAME);
+                    mAdapter.removeThemes(backendName);
+                    break;
+            }
+        }
+    };
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        final IntentFilter filter = new IntentFilter();
+        filter.addAction(BroadcastHelper.ACTION_BACKEND_CONNECTED);
+        filter.addAction(BroadcastHelper.ACTION_BACKEND_DISCONNECTED);
+        LocalBroadcastManager.getInstance(getContext())
+                .registerReceiver(mEventReceiver, filter);
+    }
+
+    @Override
+    public void onPause() {
+        LocalBroadcastManager.getInstance(getContext()).unregisterReceiver(mEventReceiver);
+        super.onPause();
     }
 }
