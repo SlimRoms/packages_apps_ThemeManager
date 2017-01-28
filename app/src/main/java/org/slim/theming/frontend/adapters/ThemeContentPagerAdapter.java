5e4964a690a91ab97a05c4c224273c2ba5d28b91
Griffin Millender <griffinn.millender@gmail.com>
2017-01-27 21:03:41 -0600

    Make groups an keyed hashmap

--- app/src/main/java/org/slim/theming/frontend/adapters/ThemeContentPagerAdapter.java
+++ app/src/main/java/org/slim/theming/frontend/adapters/ThemeContentPagerAdapter.java
@@ -3,30 +3,43 @@ package org.slim.theming.frontend.adapters;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
+
+import com.slimroms.themecore.OverlayGroup;
 import com.slimroms.themecore.OverlayThemeInfo;
 import org.slim.theming.frontend.fragments.OverlayGroupFragment;
+
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class ThemeContentPagerAdapter extends FragmentPagerAdapter {
     private OverlayThemeInfo mOverlayInfo;
 
+    private ArrayList<String> mKeys = new ArrayList<>();
+
     public ThemeContentPagerAdapter(FragmentManager fm, OverlayThemeInfo overlayInfo) {
         super(fm);
         mOverlayInfo = overlayInfo;
+
+        mKeys.addAll(mOverlayInfo.groups.keySet());
+        Collections.sort(mKeys);
+        if (mKeys.contains(OverlayGroup.OVERLAYS)) {
+            Collections.swap(mKeys, mKeys.indexOf(OverlayGroup.OVERLAYS), 0);
+        }
     }
 
     @Override
     public Fragment getItem(int position) {
         // page with overlays
-        return OverlayGroupFragment.newInstance(mOverlayInfo.groups.get(position));
+        return OverlayGroupFragment.newInstance(mOverlayInfo.groups.get(mKeys.get(position)));
     }
 
     @Override
     public int getCount() {
-        return mOverlayInfo.groups.size();
+        return mKeys.size();
     }
 
     @Override
     public CharSequence getPageTitle(int position) {
-        return mOverlayInfo.groups.get(position).title;
+        return mOverlayInfo.groups.get(mKeys.get(position)).title;
     }
 }
