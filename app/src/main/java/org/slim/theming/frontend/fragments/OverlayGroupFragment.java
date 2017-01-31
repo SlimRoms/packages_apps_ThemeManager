3b8c135afb1dbc653ee33d78621065b8ab71dd5f
Victor Lapin <victor@slimroms.org>
2017-01-31 17:43:23 +0300

    added 'select all' action to the overlays fragment

--- app/src/main/java/org/slim/theming/frontend/fragments/OverlayGroupFragment.java
+++ app/src/main/java/org/slim/theming/frontend/fragments/OverlayGroupFragment.java
@@ -3,24 +3,32 @@ package org.slim.theming.frontend.fragments;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
+import android.view.*;
+import com.slimroms.themecore.Overlay;
 import com.slimroms.themecore.OverlayGroup;
 import org.slim.theming.frontend.R;
 import org.slim.theming.frontend.adapters.OverlayGroupAdapter;
+import org.slim.theming.frontend.helpers.MenuTintHelper;
 import org.slim.theming.frontend.views.LineDividerItemDecoration;
 
 public class OverlayGroupFragment extends Fragment {
     private OverlayGroup mOverlayGroup;
+    private OverlayGroupAdapter mAdapter;
 
     public static OverlayGroupFragment newInstance(OverlayGroup group) {
         final OverlayGroupFragment fragment = new OverlayGroupFragment();
         fragment.mOverlayGroup = group;
         return fragment;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
     }
 
     @Nullable
@@ -37,7 +45,35 @@ public class OverlayGroupFragment extends Fragment {
         recycler.setLayoutManager(new LinearLayoutManager(getContext()));
         recycler.addItemDecoration(new LineDividerItemDecoration(getContext()));
         recycler.setItemAnimator(new DefaultItemAnimator());
-        final OverlayGroupAdapter adapter = new OverlayGroupAdapter(getContext(), mOverlayGroup);
-        recycler.setAdapter(adapter);
+        mAdapter = new OverlayGroupAdapter(getContext(), mOverlayGroup);
+        recycler.setAdapter(mAdapter);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.overlay_group, menu);
+    }
+
+    @Override
+    public void onPrepareOptionsMenu(Menu menu) {
+        super.onPrepareOptionsMenu(menu);
+        MenuTintHelper.tintMenu(menu, ContextCompat.getColor(getContext(), android.R.color.white));
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_select_all:
+                Boolean newValue = null;
+                for (Overlay overlay : mOverlayGroup.overlays) {
+                    if (newValue == null)
+                        newValue = !overlay.checked;
+                    overlay.checked = newValue;
+                }
+                mAdapter.notifyDataSetChanged();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
---
+++ app/src/main/java/org/slim/theming/frontend/helpers/MenuTintHelper.java
@@ -0,0 +1,23 @@
+package org.slim.theming.frontend.helpers;
+
+import android.graphics.drawable.Drawable;
+import android.support.v4.graphics.drawable.DrawableCompat;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MenuTintHelper {
+    public static void tintMenu(Menu menu, int color) {
+        for (int i = 0; i < menu.size(); ++i) {
+            final MenuItem item = menu.getItem(i);
+            if (item.isEnabled()) {
+                final Drawable drawable = item.getIcon();
+                if (drawable != null) {
+                    final Drawable wrapped = DrawableCompat.wrap(drawable);
+                    drawable.mutate();
+                    DrawableCompat.setTint(wrapped, color);
+                    item.setIcon(drawable);
+                }
+            }
+        }
+    }
+}
