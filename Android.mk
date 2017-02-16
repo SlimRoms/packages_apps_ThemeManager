704af0337fad912f9b4d56b0bae9e1520edc2916
Griffin Millender <griffinn.millender@gmail.com>
2017-02-15 22:59:22 -0600

    Compile in rom

---
+++ Android.mk
@@ -0,0 +1,38 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES := \
+    apache-commons-io:libs/commons-io-2.5.jar
+include $(BUILD_MULTI_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE_TAGS := optional
+
+LOCAL_STATIC_JAVA_LIBRARIES := \
+    android-support-v7-appcompat \
+    android-support-v4 \
+    android-support-design \
+    android-support-v7-recyclerview \
+    android-support-v7-preference \
+    android-support-v14-preference \
+    theme-core \
+    apache-commons-io
+
+LOCAL_SRC_FILES := $(call all-java-files-under, src)
+
+LOCAL_RESOURCE_DIR := \
+    $(LOCAL_PATH)/res \
+    frameworks/support/v7/appcompat/res \
+    frameworks/support/v7/recyclerview/res \
+    frameworks/support/v7/preference/res \
+    frameworks/support/v14/preference/res \
+    frameworks/support/design/res \
+    frameworks/opt/theme-core/res
+
+LOCAL_PROGUARD_ENABLED := disabled
+LOCAL_CERTIFICATE := platform
+LOCAL_PACKAGE_NAME := ThemeManager
+LOCAL_AAPT_FLAGS := --auto-add-overlay \
+    --extra-packages android.support.v7.appcompat:android.support.v7.recyclerview:android.support.v7.preference:android.support.v14.preference:android.support.design:com.slimroms.themecore
+
+include $(BUILD_PACKAGE)
