abbe3fb065ebe5cafb7806d0906ee70d06acdbba
Griffin Millender <griffinn.millender@gmail.com>
2017-02-17 09:50:50 -0600

    Move static lib declaration to core

--- Android.mk
+++ Android.mk
@@ -1,11 +1,6 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES := \
-    apache-commons-io:libs/commons-io-2.5.jar
-include $(BUILD_MULTI_PREBUILT)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
 
 LOCAL_STATIC_JAVA_LIBRARIES := \
