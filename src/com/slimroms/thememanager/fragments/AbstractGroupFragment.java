5cca533912d2df23b88f663370576ba7becbb65c
Victor Lapin <victor@slimroms.org>
2017-02-28 21:42:11 +0300

    fix scrolling lag

--- src/com/slimroms/thememanager/fragments/AbstractGroupFragment.java
+++ src/com/slimroms/thememanager/fragments/AbstractGroupFragment.java
@@ -1,14 +1,14 @@
 package com.slimroms.thememanager.fragments;
 
+import android.content.Context;
+import android.graphics.Point;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
+import android.view.*;
 import android.widget.TextView;
 import com.slimroms.themecore.OverlayGroup;
 import com.slimroms.thememanager.R;
@@ -17,8 +17,15 @@ import com.slimroms.thememanager.views.LineDividerItemDecoration;
 public abstract class AbstractGroupFragment extends Fragment {
     protected OverlayGroup mOverlayGroup;
     protected RecyclerView.Adapter mAdapter;
+    private WindowManager mWindowManager;
 
     public abstract RecyclerView.Adapter getAdapter();
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mWindowManager = (WindowManager) getContext().getSystemService(Context.WINDOW_SERVICE);
+    }
 
     @Nullable
     @Override
@@ -40,7 +47,7 @@ public abstract class AbstractGroupFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
 
         final RecyclerView recycler = (RecyclerView) view.findViewById(R.id.list);
-        recycler.setLayoutManager(new LinearLayoutManager(getContext()));
+        recycler.setLayoutManager(new ExpandedLinearLayoutManager(getContext(), mWindowManager));
         recycler.addItemDecoration(new LineDividerItemDecoration(getContext()));
         recycler.setItemAnimator(new DefaultItemAnimator());
         recycler.setHasFixedSize(true);
@@ -48,5 +55,23 @@ public abstract class AbstractGroupFragment extends Fragment {
         recycler.setAdapter(mAdapter);
         final TextView emptyView = (TextView) view.findViewById(R.id.empty_view);
         emptyView.setVisibility((mAdapter.getItemCount() == 0) ? View.VISIBLE : View.GONE);
+    }
+
+    private class ExpandedLinearLayoutManager extends LinearLayoutManager {
+        private WindowManager mWindowManager;
+
+        ExpandedLinearLayoutManager(Context context, WindowManager manager) {
+            super(context);
+            this.mWindowManager = manager;
+        }
+
+        @Override
+        protected int getExtraLayoutSpace(RecyclerView.State state) {
+            // return screen height for smooth scrolling
+            final Display display = mWindowManager.getDefaultDisplay();
+            final Point size = new Point();
+            display.getSize(size);
+            return size.y;
+        }
     }
 }
