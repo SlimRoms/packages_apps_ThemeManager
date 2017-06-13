5a814f19e716191fa374e52660227b47fb6cf247
Josue Rivera <prbassplayer@gmail.com>
2017-06-14 00:51:19 +0200

    ThemeManager: Lower perms on app cache dir
      * Since OmsBackend and Manager share UID lower perms to 700
    
    Change-Id: I32c8c690ae1ebeb7032b83b33b9aaf9b86b8fc43
    Signed-off-by: Josue Rivera <prbassplayer@gmail.com>

--- src/com/slimroms/thememanager/App.java
+++ src/com/slimroms/thememanager/App.java
@@ -153,7 +153,7 @@ public class App extends Application {
         if (!appCache.exists()) {
             if (appCache.mkdir()) {
                 try {
-                    Shell.chmod(appCache.getAbsolutePath(), 744);
+                    Shell.chmod(appCache.getAbsolutePath(), 700);
                 }
                 catch (Exception ex1) {
                     ex1.printStackTrace();
